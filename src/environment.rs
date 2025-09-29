6b4e9735a55d76f18dff3bc373f7c912ded1e8ad
Ian Gillespie <ian@flyingcouch.com>
2025-09-29 17:52:17 -0400

    environment module includes ambient light settings and a directional light

---
+++ src/environment.rs
@@ -0,0 +1,31 @@
+use ::bevy::prelude::*;
+
+const LIGHT_INTENSITY: f32 = 10_000.0;
+const AMBIENT_INTENSITY: f32 = 100.0;
+const LIGHT_TRANSLATION: Vec3 = Vec3::new(1.0, 1.0, 1.0);
+
+pub struct EnvironmentPlugin;
+impl Plugin for EnvironmentPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_lights);
+    }
+}
+
+fn spawn_lights(
+    mut commands: Commands
+) {
+    commands.spawn((
+        DirectionalLight {
+            color: Color::WHITE,
+            illuminance: LIGHT_INTENSITY,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_translation(LIGHT_TRANSLATION).looking_at(Vec3::ZERO, Vec3::Z)
+    ));
+    commands.insert_resource(AmbientLight{
+        color: Color::WHITE,
+        brightness: AMBIENT_INTENSITY,
+        ..default()
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use ::bevy::prelude::*;
 mod camera_module;
 mod player_controls;
 mod debug_scene;
+mod environment;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(camera_module::TopDownCameraPlugin)
         .add_plugins(player_controls::PlayerControlsPlugin)
+        .add_plugins(environment::EnvironmentPlugin)
         .add_plugins(debug_scene::DebugScenePlugin)
         .run();
 }
