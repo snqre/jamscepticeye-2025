09f9e02e707825a9cc5571bbadba24ac1dec69f8
Ian Gillespie <ian@flyingcouch.com>
2025-10-01 16:05:38 -0400

    added foresting algorithim demonstration

--- src/debug_scene.rs
+++ src/debug_scene.rs
@@ -1,5 +1,7 @@
+use std::fmt::Debug;
 use ::bevy::prelude::*;
 use bevy::pbr::NotShadowCaster;
+use fastrand::Rng;
 
 const TILE_DIAMETER: f32 = 1.0;
 const TILING_RADIUS: isize = 4;
@@ -9,6 +11,7 @@ pub struct DebugScenePlugin;
 impl Plugin for DebugScenePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, checker_spawn);
+        app.add_systems(Startup, foresting_algorithm);
     }
 }
 
@@ -36,5 +39,36 @@ fn checker_spawn(
                 NotShadowCaster
             ));
         }
+    }
+}
+
+fn foresting_algorithm(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    let stone_mesh = meshes.add(Sphere::new(0.125));
+    let stone_material = materials.add(StandardMaterial {
+        base_color: Color::linear_rgb(0.3, 0.3, 0.3),
+        ..default()
+    });
+    let mut rng = Rng::new();
+    let spacing = 0.5f32;
+    let max = TILING_RADIUS as f32 * TILE_DIAMETER;
+    let mut x = -max;
+    let mut y = -max;
+    let mut mmmm = true;
+    while x < max {
+        while y < max {
+            let (r1, r2, r3) = (rng.f32() - 0.5, rng.f32() - 0.5, rng.f32() + 0.5);
+            commands.spawn((
+                Mesh3d(stone_mesh.clone()),
+                MeshMaterial3d(stone_material.clone()),
+                Transform::from_xyz(x + r1, y + r2, 0.0).with_scale(Vec3::ONE * r3)
+            ));
+            y+=spacing;
+        }
+        x+=spacing;
+        y = -max;
     }
 }
