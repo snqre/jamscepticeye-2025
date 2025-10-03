37d2f9c17ff577f7c3ef73736b0e9247ff726d06
Ian Gillespie <ian@flyingcouch.com>
2025-10-03 14:10:17 -0400

    integrated animal module with working systems and a spawn event--also, macro$

--- src/animal.rs
+++ src/animal.rs
@@ -1,12 +1,65 @@
 use super::*;
 
+const ANIMAL_Z: f32 = 0.0;
+const ANIMAL_SPHERE_RADIUS: f32 = 0.25;
+const ANIMAL_SPEED: f32 = 1.0;
+const ANIMAL_DISTANCE: f32 = 10.0;
+
 pub struct AnimalPlugin;
 impl Plugin for AnimalPlugin {
     fn build(&self, app: &mut App) {
-
+        // app.add_systems(Update, Animal::sys_find_next_position);
+        // app.add_systems(Update, Animal::sys_goto_to_next_position);
+        app.add_systems(Update, Animal::animal_move_system);
+        app.add_event::<AnimalSpawn>();
+        app.add_systems(Startup, setup_animal_assets);
+        app.add_systems(Update, animal_spawn_reader.run_if(event_exists!(AnimalSpawn)));
     }
 }
 
+#[derive(Event, Copy, Clone)]
+pub struct AnimalSpawn{
+    pos: Vec2
+}
+
+fn animal_spawn_reader(
+    mut event_reader: EventReader<AnimalSpawn>,
+    mut commands: Commands,
+    animal_assets: Res<AnimalAssets>
+) {
+    for event in event_reader.read() {
+        commands.spawn((
+            Animal::new(event.pos),
+            Transform::from_translation(event.pos.extend(ANIMAL_Z)),
+            Mesh3d(animal_assets.mesh.clone()),
+            MeshMaterial3d(animal_assets.material.clone())
+        ));
+    }
+}
+
+fn setup_animal_assets(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut event_writer: EventWriter<AnimalSpawn>
+) {
+    let material = materials.add(
+        StandardMaterial {
+            base_color: Color::linear_rgb(0.0, 1.0, 0.0),
+            unlit: true,
+            ..default()
+        }
+    );
+    let mesh = meshes.add(Sphere::new(ANIMAL_SPHERE_RADIUS));
+    commands.insert_resource(AnimalAssets{mesh, material});
+    event_writer.write(AnimalSpawn{pos: Vec2::ZERO});
+}
+
+#[derive(Resource)]  // THIS IS TEMPORARY UNTIL PROPS ARE DONE
+struct AnimalAssets{
+    mesh: Handle<Mesh>,
+    material: Handle<StandardMaterial>
+}
 #[derive(Component)]
 #[require(Transform)]
 pub struct Animal {
@@ -16,10 +69,13 @@ pub struct Animal {
 }
 
 impl Animal {
-    pub fn spawn(mut commands: Commands) {
-        commands.spawn((
-            Transform::from((0.0, 0.0, 0.0))
-        ));
+
+    pub fn new(pos: Vec2) -> Self {
+        Self {
+            position: pos,
+            next_position: pos,
+            move_progress: 1.0
+        }
     }
 
     pub fn find_next_position(&mut self, distance: f32) {
@@ -29,11 +85,11 @@ impl Animal {
         self.next_position = Self::random_position(distance);
     }
 
-    pub fn goto_next_position(&mut self) {
+    pub fn goto_next_position(&mut self, dt: f32) {
         if self.position == self.next_position {
             return
         }
-        self.move_progress = self.move_progress + 0.02;
+        self.move_progress += dt * ANIMAL_SPEED;
         let t: f32 = self.move_progress.clamp(0.0, 1.0);
         let t_eased: f32 = (t * ::std::f32::consts::PI).sin().abs();
         let new_position: Vec2 = self.position.lerp(self.next_position, t_eased);
@@ -56,26 +112,41 @@ impl Animal {
 }
 
 impl Animal {
-    pub fn sys_find_next_position(query: Query<&mut Self>) {
-        for s in query.iter() {
+    pub fn animal_move_system(
+        time: Res<Time>,
+        animal_query: Query<(&mut Animal, &mut Transform)>
+    ) {
+        let dt = time.delta_secs();
+        for (mut animal, mut transform) in animal_query {
+            animal.find_next_position(ANIMAL_DISTANCE);
+            animal.goto_next_position(dt);
+            transform.translation = animal.position.extend(ANIMAL_Z);
+        }
+    }
+
+    pub fn sys_find_next_position(mut query: Query<&mut Self>) {
+        for mut s in query.iter_mut() {
             s.find_next_position(200.0);
         }
     }
 
-    pub fn sys_goto_to_next_position(query: Query<&mut Self>) {
-        for s in query.iter() {
-            s.goto_next_position();
+    pub fn sys_goto_to_next_position(mut query: Query<&mut Self>) {
+        for mut s in query.iter_mut() {
+            s.goto_next_position(0.02);
         }
     }
+
 }
 
-impl ::bevy::prelude::Plugin for Animal {
-    fn build(&self, app: &mut App) {
-        app
-            .add_systems(Update, Self::sys_find_next_position)
-            .add_systems(Update, Self::sys_goto_to_next_position);
-    }
-}
+// impl ::bevy::prelude::Plugin for Animal {
+//     fn build(&self, app: &mut App) {
+//         app.add_systems(Update, Self::sys_find_next_position);
+//         app.add_systems(Update, Self::sys_goto_to_next_position);
+//         app.add_event::<AnimalSpawn>();
+//         app.add_systems(Startup, setup_animal_assets);
+//         app.add_systems(Update, setup_animal_assets.run_if(event_exists!(AnimalSpawn)));
+//     }
+// }
 
 #[cfg(test)]
 mod test {
@@ -93,7 +164,7 @@ mod test {
             next_position: Vec2::new(100.0, 0.0),
             move_progress: 0.0
         };
-        animal.goto_next_position();
+        animal.goto_next_position(0.02);
 
         // Sorry terrible test will need better ones, but 90% chance this works, the math seams to check out.
         assert!(animal.position.x > 0.0);
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use ::bevy::prelude::*;
 
+mod macros;
 mod animal;
 mod camera_module;
 mod player_controls;
 mod debug_scene;
 mod environment;
+// mod model_previewer;
+// mod assets;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        // .add_plugins(model_previewer::ModelViewerPlugin)
         .add_plugins(camera_module::TopDownCameraPlugin)
         .add_plugins(player_controls::PlayerControlsPlugin)
         .add_plugins(environment::EnvironmentPlugin)
         .add_plugins(debug_scene::DebugScenePlugin)
+        .add_plugins(animal::AnimalPlugin)
         .run();
 }
